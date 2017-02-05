87ee79b61994a5766d75d0e23c45930c6e10fb50
ravsom <ravikanth.somayaji@gmail.com>
2017-02-05 16:11:22 +0530

    added dynamic caching to amazonaws requests

--- public/sw-import.js
+++ public/sw-import.js
@@ -11,15 +11,52 @@ if (Notification.permission !== 'granted') {
 	console.log('The user has not ---- else.');
 }
 
-navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-	serviceWorkerRegistration.pushManager.getSubscription()
-		.then(function(subscription) {
-			if (!subscription) {
-				// Set appropriate app states.
-				console.log('subscription: ' + subscription);
-			}
+self.addEventListener('push', function(event) {
+	console.log('Received a push message', event);
+
+	const title = 'Yay a message.';
+	const body = 'We have received a push message.';
+	const icon = '/images/icon-192x192.png';
+	const tag = 'simple-push-demo-notification-tag';
+
+	event.waitUntil(
+		self.registration.showNotification(title, {
+			body: body,
+			icon: icon,
+			tag: tag
 		})
-		.catch(function(err) {
-			console.log('Error during getSubscription()', err);
-		});
+	);
 });
+
+self.addEventListener('notificationclick', function(event) {
+	console.log('On notification click: ', event.notification.tag);
+	// Android doesn’t close the notification when you click on it
+	// See: http://crbug.com/463146
+	event.notification.close();
+
+	// This looks to see if the current is already open and
+	// focuses if it is
+	event.waitUntil(clients.matchAll({
+		type: 'window'
+	}).then(function(clientList) {
+		for (let i = 0; i < clientList.length; i++) {
+			const client = clientList[i];
+			if (client.url === '/' && 'focus' in client) {
+				return client.focus();
+			}
+		}
+		if (clients.openWindow) {
+			return clients.openWindow('/');
+		}
+	}));
+});
+
+console.log("serviceworker: registratoin : " + JSON.stringify(self.registration));
+console.log("serviceworker: " + JSON.stringify(self.registration));
+
+self.addEventListener('fetch', (event) => {
+	console.log('ravi\'s fetch listener - url ' + event.request.url);
+	console.log('ravi\'s fetch listener - mode ' + event.request.mode);
+	console.log('ravi\'s fetch listener - method ' + event.request.method);
+
+})
--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,8 +3,22 @@ module.exports = {
 	staticFileGlobs: [
 		'build/*.html',
 		'build/manifest.json',
+		'build/sw-import.js',
 		'build/static/**/!(*map*)'
 	],
 	dontCacheBustUrlsMatching: /\.\w{8}\./,
-	swFilePath: 'build/service-worker.js'
+	swFilePath: 'build/service-worker.js',
+	importScripts: ['sw-import.js'],
+	runtimeCaching: [{
+		urlPattern: '/(.*)',
+		handler: 'cacheFirst',
+		options: {
+			cache: {
+				name: 'amazonaws',
+				maxEntries: 10,
+				maxAgeSeconds: 86400
+			},
+			origin: /\.amazonaws\.com/
+		}
+	}]
 };
